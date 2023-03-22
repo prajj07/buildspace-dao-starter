1516baa9fc42d02a0d40a10796a3e096be25ca1c
Prajwal813793 <prajwalvm10@gmail.com>
2023-03-22 15:24:28 +0530

    intial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,67 @@
+import { useAddress, ConnectWallet, Web3Button, useContract, useNFTBalance } from '@thirdweb-dev/react';
+import { useState, useEffect, useMemo } from "react";
+
 const App = () => {
-  return (
-    <div className="landing">
-      <h1>Welcome to My DAO</h1>
+  // Use the hooks thirdweb give us.
+  const address = useAddress();
+  console.log("👋 Address:", address);
+
+  // Initialize our Edition Drop contract
+  const editionDropAddress = "0x3e98C2C2E9943fE9550786461493f8e091E09cd0"
+  const { contract: editionDrop } = useContract(editionDropAddress, "edition-drop");
+  // Hook to check if the user has our NFT
+  const { data: nftBalance } = useNFTBalance(editionDrop, address, "0")
+
+  const hasClaimedNFT = useMemo(() => {
+    return nftBalance && nftBalance.gt(0)
+  }, [nftBalance])
+
+
+  // This is the case where the user hasn't connected their wallet
+  // to your web app. Let them call connectWallet.
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to HostelDAO</h1>
+        <div className="btn-hero">
+          <ConnectWallet />
+        </div>
+      </div>
+    );
+  }
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>🍪DAO Member Page</h1>
+        <p>Congratulations on being a member</p>
+      </div>
+    );
+  };
+
+  // This is the case where we have the user's address
+  // which means they've connected their wallet to our site!
+   // Render mint nft screen.
+   return (
+    <div className="mint-nft">
+      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <div className="btn-hero">
+        <Web3Button 
+          contractAddress={editionDropAddress}
+          action={contract => {
+            contract.erc1155.claim(0, 1)
+          }}
+          onSuccess={() => {
+            console.log(`🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`);
+          }}
+          onError={error => {
+            console.error("Failed to mint NFT", error);
+          }}
+        >
+          Mint your NFT (FREE)
+        </Web3Button>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
